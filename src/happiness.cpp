188a85c2270bf92a452a32f54959412cf44d7819
Matti Lattu <matti.lattu@iki.fi>
2023-11-06 20:35:58 +0200

    Use TinyGPS++ for NEO-6M, improved interrupt loggigns

--- src/happiness.cpp
+++ src/happiness.cpp
@@ -1,4 +1,4 @@
-#include <Arduino_MKRGPS.h>
+#include <TinyGPSPlus.h>
 #include <SPI.h>
 #include <SD.h>
 #include <RTCZero.h>
@@ -11,7 +11,14 @@ int pinButtonRed = 6;
 int pinBeeper = 4;
 bool buttonPressedGreen;
 bool buttonPressedRed;
-bool firstCycle;
+bool firstLocationWrittenToLog;
+
+#define USING_TIMER_TCC true
+#include <SAMDTimerInterrupt.h>
+#define TIMER_INTERVAL_MS 60000
+#define SELECTED_TIMER TIMER_TCC
+bool timerInterruptFlagUp;
+SAMDTimer ITimer(SELECTED_TIMER);
 
 RTCZero rtc;
 
@@ -21,6 +28,10 @@ void onButtonPressedGreen() {
 
 void onButtonPressedRed() {
     buttonPressedRed = true;
+}
+
+void onTimerInterrupt() {
+    timerInterruptFlagUp = true;
 }
 
 void signalLedOff() {
@@ -75,14 +86,14 @@ void signalLedBlinkAndHalt(uint8_t errorCode, String errorMessage) {
     }
 }
 
-void appendDataFile(float latitude, float longitude, float altitude, float speed, int satellites, unsigned long unixTime, String action) {
-    unsigned long time;
+void appendDataFile(float latitude, float longitude, float altitude, float speed, int satellites, char * datetime, String action) {
+    char datetimeToWrite[40];
 
-    if (unixTime == 0) {
-        time = rtc.getSeconds() + rtc.getMinutes() * 60 + rtc.getHours() * 3600;
+    if (strcmp(datetime, "") == 0) {
+        sprintf(datetimeToWrite, "%d", rtc.getSeconds() + rtc.getMinutes() * 60 + rtc.getHours() * 3600);
     }
     else {
-        time = unixTime;
+        strcpy(datetimeToWrite, datetime);
     }
 
     File f = SD.open("data.log", FILE_WRITE);
@@ -98,7 +109,7 @@ void appendDataFile(float latitude, float longitude, float altitude, float speed
         f.print(",\"satellites\":");
         f.print(satellites);
         f.print(",\"unixTime\":");
-        f.print(time);
+        f.print(datetimeToWrite);
         f.print(",\"action\":\"");
         f.print(action);
         f.print("\"");
@@ -111,8 +122,8 @@ void appendDataFile(float latitude, float longitude, float altitude, float speed
 }
 
 void setup() {
-    // put your setup code here, to run once:
     Serial.begin(115200);
+    Serial1.begin(9600);
     delay(1000);
 
     pinMode(pinLed, OUTPUT);
@@ -133,34 +144,20 @@ void setup() {
     }
     Serial.println("OK");
 
-    if (!GPS.begin()) {
-        signalLedBlinkAndHalt(2, "Failed to initialise GPS. Halting.");
+    if (!Serial1) {
+        signalLedBlinkAndHalt(2, "No serial connection to GPS. Halting.");
     }
+
+    Serial.print("Initialising timer handler...");
+    if (!ITimer.attachInterruptInterval_MS(TIMER_INTERVAL_MS, onTimerInterrupt)) {
+        signalLedBlinkAndHalt(3, "Cannot initialise timer. Halting.");
+    }
+    Serial.println("OK");
+
     beepOff();
 
     Serial.print("Writing boot log entry...");
-    appendDataFile(0, 0, 0, 0, 0, 0, "boot");
-    Serial.println("OK");
-
-    Serial.print("Configuring GPS refresh rate...");
-    GPS.setUpdateRate(2000, 1, 1);
-    Serial.println("OK");
-
-    Serial.print("Waiting for GPS/satellites...");
-    while (GPS.satellites() == 0) {
-        GPS.available();
-        if (rtc.getSeconds() % 2 == 0) {
-            signalLedOff();
-        }
-        else {
-            signalLedOn();
-        }
-        if (rtc.getSeconds() % 15 == 0) {
-            Serial.print(".");
-            appendDataFile(0, 0, 0, 0, 0, 0, "boot-wait-gps");
-            delay(1000);
-        }
-    }
+    appendDataFile(0, 0, 0, 0, 0, "", "boot");
     Serial.println("OK");
 
     pinMode(pinButtonGreen, INPUT_PULLUP);
@@ -171,79 +168,114 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(pinButtonRed), onButtonPressedRed, FALLING);
     buttonPressedRed = false;
 
-    firstCycle = true;
+    firstLocationWrittenToLog = false;
 }
 
+TinyGPSPlus gps;
 uint8_t lastGpsAvailableMinutes;
 uint8_t lastGpsUnavailableMinutes;
 
+float latitude = 0;
+float longitude = 0;
+float altitude = 0;
+float speed = 0;
+uint32_t satellites = 0;
+uint32_t unixTime = 0;
+char date[20];
+char time[20];
+char datetime[40];
+
 void loop() {
-    GPS.available();
-    
-    float latitude = GPS.latitude();
-    float longitude = GPS.longitude();
-    float altitude = GPS.altitude();
-    float speed = GPS.speed();
-    int satellites = GPS.satellites();
-    unsigned long unixTime = GPS.getTime();
+    while (Serial1.available() > 0) {
+        gps.encode(Serial1.read());
 
-    if (satellites != 0) {
-        signalLedOn();
-
-        Serial.print("Location: ");
-        Serial.print(latitude, 7);
-        Serial.print(", ");
-        Serial.print(longitude, 7);
-        Serial.print("  Alt: ");
-        Serial.print(altitude, 7);
-        Serial.print("  Speed: ");
-        Serial.print(speed, 2);
-        Serial.print("  Satellites: ");
-        Serial.print(satellites);
-        Serial.print("  Time: ");
-        Serial.println(unixTime);
-
-        if (firstCycle) {
-            appendDataFile(latitude, longitude, altitude, speed, satellites, unixTime, "boot");
-            Serial.println("Boot entry was written to log");
-            firstCycle = false;
+        if (gps.location.isUpdated()) {
+            latitude = gps.location.lat();
+            longitude = gps.location.lng();
         }
 
-        if (buttonPressedGreen) {
-            beepOn();
-            appendDataFile(latitude, longitude, altitude, speed, satellites, unixTime, "green");
-            Serial.println("Button: Green");
-            signalLedOff();
-            delay(1000);
-            buttonPressedGreen = false;
-            beepOff();
+        if (gps.altitude.isUpdated()) {
+            altitude = gps.altitude.meters();
         }
 
-        if (buttonPressedRed) {
-            beepOn();
-            appendDataFile(latitude, longitude, altitude, speed, satellites, unixTime, "red");
-            Serial.println("Button: Red");
-            signalLedOff();
-            delay(1000);
-            buttonPressedRed = false;
-            beepOff();
+        if (gps.speed.isUpdated()) {
+            speed = gps.speed.kmph();
         }
 
-        if (rtc.getMinutes() % 15 == 0) {
-            if (rtc.getMinutes() != lastGpsAvailableMinutes) {
-                appendDataFile(latitude, longitude, altitude, speed, satellites, unixTime, "gps-available");
-                lastGpsAvailableMinutes = rtc.getMinutes();
+        if (gps.satellites.isUpdated()) {
+            satellites = gps.satellites.value();
+        }
+
+        if (gps.date.isUpdated()) {
+            sprintf(date, "%04d-%02d-%02d", gps.date.year(), gps.date.month(), gps.date.day());
+            sprintf(datetime, "%s %s", date, time);
+        }
+
+        if (gps.time.isUpdated()) {
+            sprintf(time, "%02d:%02d:%02d", gps.time.hour(), gps.time.minute(), gps.time.second());
+            sprintf(datetime, "%s %s", date, time);
+        }
+
+        if (satellites != 0 && latitude != 0 && longitude != 0) {
+            signalLedOn();
+
+            Serial.print("Location: ");
+            Serial.print(latitude, 7);
+            Serial.print(", ");
+            Serial.print(longitude, 7);
+            Serial.print("  Alt: ");
+            Serial.print(altitude, 7);
+            Serial.print("  Speed: ");
+            Serial.print(speed, 2);
+            Serial.print("  Satellites: ");
+            Serial.print(satellites);
+            Serial.print("  Time: ");
+            Serial.println(datetime);
+
+            if (!firstLocationWrittenToLog) {
+                appendDataFile(latitude, longitude, altitude, speed, satellites, datetime, "boot");
+                Serial.println("Boot entry was written to log");
+                firstLocationWrittenToLog = true;
+            }
+
+            if (buttonPressedGreen) {
+                beepOn();
+                appendDataFile(latitude, longitude, altitude, speed, satellites, datetime, "green");
+                Serial.println("Button: Green");
+                signalLedOff();
+                delay(1000);
+                buttonPressedGreen = false;
+                beepOff();
+            }
+
+            if (buttonPressedRed) {
+                beepOn();
+                appendDataFile(latitude, longitude, altitude, speed, satellites, datetime, "red");
+                Serial.println("Button: Red");
+                signalLedOff();
+                delay(1000);
+                buttonPressedRed = false;
+                beepOff();
+            }
+
+            if (timerInterruptFlagUp) {
+                appendDataFile(latitude, longitude, altitude, speed, satellites, datetime, "gps-available");
+                timerInterruptFlagUp = false;
             }
         }
-    }
-    else {
-        signalLedOff();
+        else {
+            signalLedOff();
 
-        if (rtc.getMinutes() % 15 == 0) {
-            if (rtc.getMinutes() != lastGpsUnavailableMinutes) {
-                appendDataFile(0, 0, 0, 0, 0, 0, "gps-unavailable");
-                lastGpsUnavailableMinutes = rtc.getMinutes();
+            if (timerInterruptFlagUp) {
+                appendDataFile(0, 0, 0, 0, 0, "", "gps-unavailable");
+                timerInterruptFlagUp = false;
+
+                signalLedOn();
+                delay(3000);
+                signalLedOff();
             }
+
+            Serial.print("No satellites/location\r");
         }
     }
 }
